9b3a51f1e2cdd89ef34c95c71b47a032f55701be
Khawaja Usama Yasir <66178232+khusamayasir@users.noreply.github.com>
2022-05-28 11:13:59 +0500

    Update Sidebar.js

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -2,22 +2,29 @@ import React from "react"
 import "./Sidebar.css"
 import plusIcon from "../../assets/plus-icon.png"
 
-export default function Sidebar() {
-  const colors = ["#fe9b72", "#fec971", " #00d4fe", "#b693fd", "#e4ee91"];
+export default function Sidebar(props) {
+
+  const colours = ["#fe9b72", "#fec971", " #00d4fe", "#b693fd", "#e4ee91"];
+
+  const [listOpen, setListOpen] = React.useState(false);
+
+  function toggleList(){
+    setListOpen(prevState => !prevState)
+  }
 
   return (
     <div className="sidebar">
-      <img src={plusIcon} alt="AddNote" />
-      <ul className="sidebar-list">
-        {
-          colors.map((item,index) => 
-          <li 
-          key={index} 
-          className="sidebar-list-item" 
-          style={{backgroundColor:item}}/>)
-        }
-        </ul>
-        
+      <img src={plusIcon} alt="Add" onClick={toggleList} />
+      <ul className={`sidebar-list ${listOpen ? "sidebar-list_active" : ""}`}>
+        {colours.map((color, index) => (
+          <li
+            key={index}
+            className="sidebar-list-item"
+            style={{ backgroundColor: color }}
+            onClick={() => props.addNote(color)}
+          />
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
