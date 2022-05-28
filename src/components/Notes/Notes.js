074ac88343f1c33b61cdf0c473f0de69b9165187
Khawaja Usama Yasir <66178232+khusamayasir@users.noreply.github.com>
2022-05-28 13:59:08 +0500

    Update Note.js

--- src/components/Notes/Notes.js
+++ src/components/Notes/Notes.js
@@ -1,11 +1,15 @@
 import React from "react"
 import "./Notes.css";
+import deleteIcon from "../../assets/delete-icon.png"
 
 export default function Notes(props) {
   return (
     <div className= "note" style={{backgroundColor:props.note.color}}>
         <textarea className="note-text" defaultValue={props.note.text} />
-        <p>{props.note.time}</p>        
+        <div className="note-footer">
+        <p>{formatDate(props.note.time)}</p>        
+        <img src={deleteIcon} alt="DELETE" />
+        </div>
     </div>
   )
 }
