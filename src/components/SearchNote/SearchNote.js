218b46c8f19e266cca2f1989bcea6c2a4571582f
Khawaja Usama Yasir <66178232+khusamayasir@users.noreply.github.com>
2022-05-28 17:36:13 +0500

    Update SearchNote.js

--- src/components/SearchNote/SearchNote.js
+++ src/components/SearchNote/SearchNote.js
@@ -1,7 +1,17 @@
-import React from 'react'
+import React from "react"
+import { MdSearch } from "react-icons/md";
 
-export default function SearchNote() {
-  return (
-    <div>SearchNote</div>
-  )
+const Search = ({ handleSearchNote }) => {
+	return (
+		<div className = "search">
+			<MdSearch className='search-icon' size='1.3em' />
+			<input
+				onChange={(event) => handleSearchNote(event.target.value)}
+				type = "text"
+				placeholder = "type to search..."
+			/>
+		</div>
+	)
 }
+
+export default Search;
