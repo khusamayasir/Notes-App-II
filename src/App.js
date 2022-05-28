87f72d8761b381c4c4ccf417ebd8fbb42b97cc2e
Khawaja Usama Yasir <66178232+khusamayasir@users.noreply.github.com>
2022-05-28 11:02:32 +0500

    Update Sidebar.css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react"
 import NotesEditor from "./components/NotesEditor/NotesEditor.js"
+import Sidebar from "./components/Sidebar/Sidebar.js"
 
 export default function App() {
   return (
     <div className="App">
+      <Sidebar />
       <NotesEditor />
     </div>
   )
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -3,12 +3,21 @@ import "./Sidebar.css"
 import plusIcon from "../../assets/plus-icon.png"
 
 export default function Sidebar() {
+  const colors = ["#fe9b72", "#fec971", " #00d4fe", "#b693fd", "#e4ee91"];
+
   return (
-    <div className="Sidebar">
+    <div className="sidebar">
       <img src={plusIcon} alt="AddNote" />
       <ul className="sidebar-list">
-        <li className="sidebar-list-item"></li>
-      </ul>
+        {
+          colors.map((item,index) => 
+          <li 
+          key={index} 
+          className="sidebar-list-item" 
+          style={{backgroundColor:item}}/>)
+        }
+        </ul>
+        
     </div>
   )
 }
