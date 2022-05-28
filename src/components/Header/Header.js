f63ef0d5f289bed9b7f2b2b03f5bffeff7d5edcf
Khawaja Usama Yasir <66178232+khusamayasir@users.noreply.github.com>
2022-05-28 16:04:51 +0500

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from "react"
 
-export default function header() {
-  return (
-    <div>header</div>
-  )
+const Header = ({ handleToggleDarkMode }) => {
+	return (
+		<div className="header">
+			<h1>Notes</h1>
+			<button className="btn" onClick={() => handleToggleDarkMode((previousDarkMode) => !previousDarkMode)}>
+				Toggle Mode
+			</button>
+		</div>
+	)
 }
+
+export default Header;
