55dd191fb48c31b30f95782a25d273bbacba6c2d
Khawaja Usama Yasir <66178232+khusamayasir@users.noreply.github.com>
2022-05-28 10:38:05 +0500

    Update Sidebar.js

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from "react"
+import "./Sidebar.css"
+import plusIcon from "../../assets/plus-icon.png"
 
 export default function Sidebar() {
   return (
-    <div>Sidebar</div>
+    <div className="Sidebar">
+      <img src={plusIcon} alt="AddNote" />
+      <ul className="sidebar-list">
+        <li className="sidebar-list-item"></li>
+      </ul>
+    </div>
   )
 }
