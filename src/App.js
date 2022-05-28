79df7e79ca1b29d1e542db1d67b95b5ceb4b4fce
Khawaja Usama Yasir <66178232+khusamayasir@users.noreply.github.com>
2022-05-28 13:41:05 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,57 @@
 import React from "react"
+import "./App.css"
 import NotesEditor from "./components/NotesEditor/NotesEditor.js"
 import Sidebar from "./components/Sidebar/Sidebar.js"
 
 export default function App() {
+    const [notes, setNotes] = React.useState(
+      JSON.parse(localStorage.getItem("notes-app")) || []
+    );
+  
+    const addNote = (color) => {
+      const tempNotes = [...notes];
+  
+      tempNotes.push({
+        id: Date.now() + "" + Math.floor(Math.random() * 78),
+        text: "",
+        time: Date.now(),
+        color: color,
+      });
+      setNotes(tempNotes);
+    };
+  
+    const deleteNote = (id) => {
+      const tempNotes = [...notes];
+  
+      const index = tempNotes.findIndex((item) => item.id === id);
+      if (index < 0) return;
+  
+      tempNotes.splice(index, 1);
+      setNotes(tempNotes);
+    };
+  
+    const updateText = (text, id) => {
+      const tempNotes = [...notes];
+  
+      const index = tempNotes.findIndex((item) => item.id === id);
+      if (index < 0) return;
+  
+      tempNotes[index].text = text;
+      setNotes(tempNotes);
+    };
+  
+    React.useEffect(() => {
+      localStorage.setItem("notes-app", JSON.stringify(notes));
+    }, [notes]);
+
   return (
     <div className="App">
-      <Sidebar />
-      <NotesEditor />
+      <Sidebar addNote={addNote} />
+      <NotesEditor
+        notes={notes}
+        deleteNote={deleteNote}
+        updateText={updateText}
+      />
     </div>
   )
 }
